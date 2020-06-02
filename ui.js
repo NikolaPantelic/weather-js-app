bbf2d4963ae55eacf661ef621e022cd08d213bb7
Nikola Pantelic <0nikolapantelic0@gmail.com>
2020-06-02 11:53:33 +0200

    working on ui component

---
+++ ui.js
@@ -0,0 +1,22 @@
+class Ui {
+    constructor(){
+        this.location = document.getElementById("w-location");
+        this.description = document.getElementById("w-desc");
+        this.temperature = document.getElementById("w-temp");
+        this.humidity = document.getElementById("w-humidity");
+        this.maxtemperature = document.getElementById("w-tempmax");
+        this.mintemperature = document.getElementById("w-tempmin");
+        this.feelslike = document.getElementById("w-feelslike");
+        this.windspeed = document.getElementById("w-wind");
+    }
+    paint(weather){
+        this.location.textContent = weather.name;
+        this.description.textContent = weather.weather[0].description;
+        this.temperature.textContent = `Temperature : ${weather.main.temp.toFixed()} °C`;
+        this.humidity.textContent = `Relative Humidity: ${weather.main.humidity} %`;
+        this.tempmax.textContent = `Max Temp : ${weather.main.temp_max.toFixed()} °C`;
+        this.tempmin.textContent = `Min Temp : ${weather.main.temp_min.toFixed()} °C`;
+        this.feelslike.textContent = `Feels Like : ${weather.main.feels_like.toFixed()} °C`;
+        this.wind.textContent = `Wind speed : ${weather.wind.speed} m/s`;
+    }
+}
